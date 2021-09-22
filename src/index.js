ab11e43faa38e54b1089fb9172f6d99e9e9fb145
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-09-22 10:59:55 -0400

    Create the array of objects

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+
+const todoTasks = [
+  {
+    description: 'Study React Native',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Go to the supermarket',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Do my Microverse tasks',
+    completed: false,
+    index: 1,
+  },
+];
+
+
