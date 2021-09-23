0ca1c08cc1658aee73d3eb20e420d17994d99414
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-09-22 22:25:16 -0400

    Remove the localStorage file since it was causing side effects

--- src/changeStatus.js
+++ src/changeStatus.js
@@ -1,28 +1,5 @@
 // import { setLocalStorage } from './localStorage.js';
 
-const checkboxHandler = (todoTasks, checkbox) => {
-  const checkedTask = todoTasks.find((task) => {
-    if(task.index === +checkbox.target.parentNode.id) {
-      console.log(`task.index ${task.index} checkbox.target.parentNode.id ${+checkbox.target.parentNode.id}`)
-      return task.index === +checkbox.target.parentNode.id;
-    }
-  });
-  if(checkedTask.completed) {
-    checkedTask.completed = false;
-  } else {
-    checkedTask.completed = true;
-  }
-  checkbox.target.nextElementSibling.classList.toggle('line-through');
-  getLocalStorage();
-  setLocalStorage();
-}
 
-const changeStatus = (todoTasks) => {
-  const checkboxes = document.querySelectorAll('.input-checkbox');
 
-  checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('change', checkboxHandler.bind(this, todoTasks));
-  });
-};
-
-export default changeStatus;
+// export default changeStatus;
